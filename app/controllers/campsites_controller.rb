f6c8581d544b0064a64e61d758746b40b1a59c2d
Hugo Speak <hugo.speak@hotmail.com>
2021-06-01 12:51:52 +0100

    add campsite CONTrOLLER + Updated ROUTES

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -1,2 +1,32 @@
-class CampsitesController < ApplicationControllerx
+class CampsitesController < ApplicationController
+
+  def index
+    @campsites = Campsite.all
+  end
+
+  def show
+    @campsite = Campsite.find(params[:id])
+  end
+
+  def new
+    @campsite = Campsite.new
+  end
+
+  def create
+    @campsite = Campsite.new(campsite_params)
+    @campsite.user = current_user
+    if @campsite.save
+      redirect_to campsite_path(@campsite)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def campsite_params
+    params.require(:campsite).permit(:name, :address, :description, :price_per_night, :capacity)
+  end
+
+
 end
