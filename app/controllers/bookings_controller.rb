41e9c19e3707257347410ef79d832ea2afbe4af9
Afsaneh SametTehrani <afsaneh.samettehrani@essec.edu>
2021-06-01 17:29:15 +0100

    added booking views + controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @campsite = Campsite.find(params[:campsite_id])
+    @booking.campsite = @campsite
+    numb_days =  (@booking.end_date - @booking.start_date).to_i
+    @booking.total_price = @campsite.price_per_night * @booking.number_of_guest * numb_days
+    if @booking.save
+      redirect_to my_account_path(current_user)
+    else
+      render "campsites/show"
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :number_of_guest, :status)
+  end
+
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :campsite
   belongs_to :user
 
-  # validates :start_date, presence: true
-  # validates :end_date, presence: true
-  # validates :number_of_guest, numericality: { only_integer: true }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :number_of_guest, numericality: { only_integer: true }
 end
