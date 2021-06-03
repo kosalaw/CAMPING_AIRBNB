53bbe69db98cd91d3a03e8bf874df08d45f23ae4
Hugo Speak <hugo.speak@hotmail.com>
2021-06-03 17:22:25 +0100

    Map and and markers working

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -4,10 +4,10 @@ class Campsite < ApplicationRecord
   has_one_attached :photo
 
   validates :name, :address, :description, presence: true
-  # validates :address, presence: true
-  # validates :description, presence: true
-
   validates :price_per_night, numericality: true
-
   validates :capacity, numericality: { only_integer: true }
+
+  # geocode for map
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
